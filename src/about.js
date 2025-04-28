15a2f3f0b46e8ae746eb815aaecc634ff52cbb20
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-04-28 14:11:06 +0700

    implement and style about content

--- src/about.js
+++ src/about.js
@@ -17,11 +17,16 @@ export default function renderPage() {
   <main>
     <section id="about">
       <div id="side-images">
+        <img src=${about1} alt=""/>
+        <img src=${about2} alt=""/>
+        <img src=${about3} alt=""/>
+        <img src=${about4} alt=""/>
       </div>
       <div id="about-content">
-        <h3>About Us<h3>
+        <h3>About Us</h3>
         <p>Salami shankle swine burgdoggen pig jowl pork. Chuck alcatra shoulder filet mignon jerky ball tip rump porchetta kielbasa meatball salami.</p>
         <p>Flank andouille shoulder short ribs burgdoggen beef ribs ham hock cupim turducken corned beef capicola chislic jowl. Shankle swine short ribs doner.</p>
+        <button class="about-read-more-button">READ MORE</button>
       </div>
     </section>
   </main>
