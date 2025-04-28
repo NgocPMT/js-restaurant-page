927e76baf12e9cf63fc0bd8ab7f01871fd61f50f
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-04-28 13:48:42 +0700

    import about images to about.js

--- src/about.js
+++ src/about.js
@@ -1,4 +1,8 @@
 import titleBackground from "./img/bg-hero.jpg";
+import about1 from "./img/about-1.jpg";
+import about2 from "./img/about-2.jpg";
+import about3 from "./img/about-3.jpg";
+import about4 from "./img/about-4.jpg";
 import "./css/about.css";
 
 export default function renderPage() {
@@ -10,5 +14,16 @@ export default function renderPage() {
   <section id="about-title">
     <h1 class="about-title-heading">About Us</h1>
   </section>
+  <main>
+    <section id="about">
+      <div id="side-images">
+      </div>
+      <div id="about-content">
+        <h3>About Us<h3>
+        <p>Salami shankle swine burgdoggen pig jowl pork. Chuck alcatra shoulder filet mignon jerky ball tip rump porchetta kielbasa meatball salami.</p>
+        <p>Flank andouille shoulder short ribs burgdoggen beef ribs ham hock cupim turducken corned beef capicola chislic jowl. Shankle swine short ribs doner.</p>
+      </div>
+    </section>
+  </main>
   `;
 }
