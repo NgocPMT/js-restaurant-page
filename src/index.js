1c80ee433462e8d1d0a0a769d728243332598d3f
NgocPMT <149455563+NgocPMT@users.noreply.github.com>
2025-04-28 14:13:34 +0700

    fix: about navigation button have the same id as about section

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import renderHome from "./home";
 import renderAbout from "./about";
 import "./css/style.css";
 
-const homeBtn = document.querySelector("#home");
-const aboutBtn = document.querySelector("#about");
+const homeBtn = document.querySelector("#home-btn");
+const aboutBtn = document.querySelector("#about-btn");
 
 renderHome();
 homeBtn.addEventListener("click", () => renderHome());
